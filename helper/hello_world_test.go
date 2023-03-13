375d6a077e6fb76bc83bb6153aae8fff9ddff618
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-03-13 20:09:12 +0700

    fix error test log

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -2,11 +2,20 @@ package helper
 
 import "testing"
 
-func TestHello(t *testing.T) {
+func TestHelloFadhil(t *testing.T) {
 	result := Hello("Fadhil")
 
 	if result != "Hello Fadhil" {
-		//error
-		panic("Result is not 'Hello Fadhil'")
+		// error
+		t.Error("Result is not Hello Fadhil")
+	}
+}
+
+func TestHelloKunK(t *testing.T) {
+	result := Hello("KunK")
+
+	if result != "Hello KunK" {
+		// error
+		t.Fatal("Result is not Hello KunK")
 	}
 }
