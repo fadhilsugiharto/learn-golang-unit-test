6b7d3ee937fc7e7f61ea87d3593a3e013a7bd5d1
Fadhil Sugiharto <hanun.sugiharto@pactindo.com>
2023-03-13 15:06:34 +0700

    make simple unit test program

---
+++ helper/hello_world_test.go
@@ -0,0 +1,12 @@
+package helper
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	result := Hello("Fadhil")
+
+	if result != "Hello Fadhil" {
+		// error
+		panic("Result is not 'Hello Fadhil'")
+	}
+}
