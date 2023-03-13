c6f529fd2d7395ab3343361b982362c53693d0e3
Fadhil Sugiharto <fadhilsugiharto@gmail.com>
2023-03-13 20:25:42 +0700

    add testify unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,25 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestHelloAssert(t *testing.T) {
+	result := Hello("Fadhil")
+
+	assert.Equal(t, result, "Hello Fadhil", "Result must be 'Hello Fadhil'")
+	fmt.Println("TestHelloAssert Finished")
+}
+
+func TestHelloRequire(t *testing.T) {
+	result := Hello("Fadhil")
+
+	require.Equal(t, result, "Hello Fadhil", "Result must be 'Hello Fadhil'")
+	fmt.Println("TestHelloRequire Finished")
+}
 
 func TestHelloFadhil(t *testing.T) {
 	result := Hello("Fadhil")
